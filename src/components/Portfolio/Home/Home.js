79a26b80787dfc05e0cd0514d64ca391bc1f7788
JulietteGoulam <goulamjuliette@gmail.com>
2023-08-24 23:19:29 +0200

    start to create home component

---
+++ src/components/Portfolio/Home/Home.js
@@ -0,0 +1,18 @@
+import './Home.scss';
+
+function Home(){
+    return (
+        <div className="home">
+        <div className="home-content">
+            <h2 className="home-content-title">Hello World, </h2>
+            <p className="home-content-text">bienvenue sur mon portfolio,<br/> je suis Juliette Goulam, <br/><span className="home-content-underline">développeuse web</span> et <span className="home-content-underline">designer graphique</span></p>
+        </div>
+        <div className='home-shape'>
+            
+        </div>
+        </div>
+        
+    );
+}
+
+export default Home;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,13 @@
 import './Portfolio.scss'
 
 import Header from './Header/Header';
+import Home from './Home/Home';
 
 function Portfolio() {
     return(
         <div className="portfolio">
             <Header/>
+            <Home/>
         </div>
     );
 }
